317127d16d17d3e6500c6a0232ce6314e0cb9b38
raziyahya <86314686+raziyahya@users.noreply.github.com>
2021-06-26 17:51:46 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Info(){
+  return(
+<div className="note">
+  <h1>JavaScript and React.js</h1>
+  <p>Bootcamp on basic web dev using React JS</p>
+  </div>
+  );
+
+}
+
+export default Info;
